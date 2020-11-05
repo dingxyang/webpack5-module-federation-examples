6d7ac3fd681d90241b165f659a70be847676baa5
dingxiangyang <ding.xiangyang@iwhalecloud.com>
2020-11-05 15:51:15 +0800

    shared-dynamic-routes

---
+++ shared-dynamic-routes/app1/src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const HomePage = React.lazy(() => import("./HomePage"));
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage,
+    exact: true,
+  },
+];
+
+export default routes;
