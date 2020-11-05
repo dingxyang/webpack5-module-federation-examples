69034cf6e42d456f6e6cce7d9b243b41886c74ae
dingxiangyang <ding.xiangyang@iwhalecloud.com>
2020-11-05 10:57:09 +0800

    init dynamic-system-host

---
+++ dynamic-system-host/app2/webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container
+  .ModuleFederationPlugin;
+const path = require("path");
+const deps = require("./package.json").dependencies;
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 3002,
+  },
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app2",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Widget": "./src/Widget",
+      },
+      shared: [
+        {
+          react: { singleton: true },
+          "react-dom": { singleton: true },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
